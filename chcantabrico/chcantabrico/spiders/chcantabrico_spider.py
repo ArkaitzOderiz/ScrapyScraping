1bb69f5db509f5f02778088c6c3cb259bc97365b
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-02-21 18:36:19 +0100

    Primera version funcional con Scrapy con la que sacar el nombre de la estacion y la url

--- chcantabrico/chcantabrico/spiders/chcantabrico_spider.py
+++ chcantabrico/chcantabrico/spiders/chcantabrico_spider.py
@@ -4,14 +4,26 @@ import scrapy
 class ChcantabricoSpiderSpider(scrapy.Spider):
     name = "chcantabrico_spider"
     allowed_domains = ["www.chcantabrico.es"]
-    start_urls = ["http://www.chcantabrico.es/nivel-de-los-rios"]
+    start_urls = ["https://www.chcantabrico.es/nivel-de-los-rios"]
 
     def parse(self, response):
         rows = response.xpath('//table[@class="tablefixedheader niveles"]/tbody/tr');
 
         for row in rows:
-            path = row.xpath('./td/a/@href').getall();
-            for url in path:
+            paths = row.xpath('./td/a/@href').getall();
+            estaciones = row.xpath('./td/a/text()').getall();
+            if(len(paths) == 4):
                 yield {
-                    'url': url,
+                    'estacion': estaciones[2],
+                    'url': paths[2],
+                }
+            elif(len(paths) == 3):
+                yield {
+                    'estacion': estaciones[1],
+                    'url': paths[1],
+                }
+            elif(len(paths) == 2):
+                yield {
+                    'estacion': estaciones[0],
+                    'url': paths[0],
                 }
