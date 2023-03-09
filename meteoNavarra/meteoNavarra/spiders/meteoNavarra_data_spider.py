a46c974dc05dbebed6c3166ba9a84cb56149722b
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-03-09 10:20:58 +0100

    Añadidos datos de: temperatura, humedad relativa, rediacion global

--- meteoNavarra/meteoNavarra/spiders/meteoNavarra_data_spider.py
+++ meteoNavarra/meteoNavarra/spiders/meteoNavarra_data_spider.py
@@ -20,25 +20,28 @@ class MeteonavarraDataSpiderSpider(scrapy.Spider):
             data = json.load(f)
             for estacion in data:
                 if(estacion["tipo"] == "'AUTO'"):
-                    url = f'http://meteo.navarra.es/estaciones/estacion_datos_m.cfm?IDEstacion={estacion["codigo"]}&p_10=11&fecha_desde={yesterday_date_format}&fecha_hasta={tomorrow_date_format}'
+                    url = f'http://meteo.navarra.es/estaciones/estacion_datos_m.cfm?IDEstacion={estacion["codigo"]}&p_10=1&p_10=2&p_10=3&p_10=11&fecha_desde={yesterday_date_format}&fecha_hasta={tomorrow_date_format}'
                     yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
         rows = response.xpath('//table[@class="border"]/tr[not(@bgcolor="#FFFFFF")]')
         estacion = response.xpath('//div[@class="tex"]/table/tr/td/h1[1]/text()').get()
 
-        precipitaciones = []
+        datos = []
         for row in rows:
             dato = {
                 'fecha': row.xpath('./td[1]/text()').get().strip().split(' ')[0],
                 'hora': row.xpath('./td[1]/text()').get().strip().split(' ')[1],
-                'precipitacion': row.xpath('./td/font/text()').get(),
+                'temperatura (ªC)': row.xpath('./td[2]/font/text()').get(),
+                'humedad relativa (%)': row.xpath('./td[3]/font/text()').get(),
+                'radiacion global (W/m^2)': row.xpath('./td[4]/font/text()').get(),
+                'precipitacion (l/mm^2)': row.xpath('./td[5]/font/text()').get(),
             }
 
-            if(dato['precipitacion'] != '- -'):
-                precipitaciones.append(dato)
+            if(dato['precipitacion (l/mm^2)'] != '- -'):
+                datos.append(dato)
 
         yield {
             'estacion': estacion,
-            'precipitaciones': precipitaciones,
+            'datos': datos,
         }
