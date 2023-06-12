733e4411aa1123b851fd79e2a80b26175f91aae2
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-06-12 13:12:08 +0200

    diferencia_JSON.py universal para JSON Formateados

--- DiferenciarJSON/diferencia_JSON.py
+++ DiferenciarJSON/diferencia_JSON.py
@@ -1,4 +1,5 @@
 import json
+from datetime import date, datetime
 
 with open("datos_aemet1.json", "r", encoding="utf-8") as f1:
     file1 = json.loads(f1.read())
@@ -7,15 +8,20 @@ with open("datos_aemet2.json", "r", encoding="utf-8") as f2:
 
 file3 = []
 
+fechaActual = date.today().strftime("%d/%m/%Y")
+
 for i, item in enumerate(file1):
     repeat = []
     for data in item["datos"]:
-        if data["hora"] not in [x["hora"] for x in file2[i]["datos"]]:
+        if data["fecha y hora"] not in [x["fecha y hora"] for x in file2[i]["datos"]]:
             print(f"Found difference: {data}")
-            repeat.append(data)
+            fechaDato = datetime.strptime(data["fecha y hora"], "%d/%m/%Y %H:%M:%S").date().strftime("%d/%m/%Y")
+            if fechaActual == fechaDato:
+                repeat.append(data)
     if repeat:
         file3.append({
-            "municipio": item["municipio"],
+            "coordenadas": item["coordenadas"],
+            "estacion": item["estacion"],
             "dato": repeat})
 
 print(file3)
