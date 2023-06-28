a4c2a3955e37c98fd218b76222457450fd2d6232
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-06-28 13:44:19 +0200

    Unificacion de spiders de chcantabrico para ejecucion mediante script

---
+++ Scrapy/datosEstaciones/datosEstaciones/spiders/chcantabrico_coord_spider.py
@@ -0,0 +1,33 @@
+import json
+
+import scrapy
+
+
+class ChcantabricoCoordSpiderSpider(scrapy.Spider):
+    name = "chcantabrico_coord_spider"
+    allowed_domains = ["ceh.cedex.es"]
+    custom_settings = {
+        'FEEDS': {
+            '../../JSONs/RawData/coordenadas_chcantabrico.json': {
+                'format': 'json',
+                'overwrite': True,
+            }
+        }
+    }
+
+    def start_requests(self):
+        with open("../../JSONs/Codigos/codigos_chcantabrico.json", encoding="utf-8") as f:
+            data = json.load(f)
+            for estacion in data:
+                url = f'https://ceh.cedex.es/anuarioaforos/afo/estaf-datos.asp?indroea={estacion["codigoSecundario"]}'
+                yield scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+        longitud = response.css('p::text')[6].get().strip()
+        latitud = response.css('p::text')[7].get().strip()
+        estacion = response.css('font::text')[14].get().strip()
+
+        yield {
+            'coordenadas': f'Lat: {latitud} | Lon: {longitud}',
+            'estacion': estacion,
+        }
---
+++ Scrapy/datosEstaciones/datosEstaciones/spiders/chcantabrico_pluvio_spider.py
@@ -0,0 +1,52 @@
+import io
+import json
+
+import pandas as pd
+import scrapy
+
+
+class ChcantabricoCoordSpiderSpider(scrapy.Spider):
+    name = "chcantabrico_pluvio_spider"
+    allowed_domains = ["www.chcantabrico.es"]
+    custom_settings = {
+        'FEEDS': {
+            '../../JSONs/RawData/datos_pluvio_chcantabrico.json': {
+                'format': 'json',
+                'overwrite': True,
+            }
+        }
+    }
+
+    def start_requests(self):
+        with open("../../JSONs/Codigos/codigos_chcantabrico.json", encoding="utf-8") as f:
+            data = json.load(f)
+            for estacion in data:
+                params_pluvio = {
+                    'p_p_id': 'GraficaEstacion_INSTANCE_ND81Xo17PIZ7',
+                    'p_p_lifecycle': '2',
+                    'p_p_state': 'normal',
+                    'p_p_mode': 'view',
+                    'p_p_resource_id': 'downloadCsvPluvio',
+                    'p_p_cacheability': 'cacheLevelPage',
+                    '_GraficaEstacion_INSTANCE_ND81Xo17PIZ7_cod_estacion': f'{estacion["codigo"]}',
+                    '_GraficaEstacion_INSTANCE_ND81Xo17PIZ7_tipodato': 'pluvio',
+                }
+                url = 'https://www.chcantabrico.es/precipitacion-acumulada'
+                yield scrapy.FormRequest(url=url,
+                                         method='GET',
+                                         formdata=params_pluvio,
+                                         callback=self.parse,
+                                         cb_kwargs={'estacion': estacion['codigo']}
+                )
+
+    def parse(self, response, estacion):
+        if not response.text.startswith('-'):
+            urlData = response.text
+            rawData = pd.read_csv(io.StringIO(urlData), delimiter=';', encoding='utf-8', header=1)
+            rawData.columns = ['fecha y hora', 'nivel (m)']
+            parsedData = rawData.to_json(orient="records")
+
+            yield {
+                'estacion': estacion,
+                'datos': json.loads(parsedData)
+            }
