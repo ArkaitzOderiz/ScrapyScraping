b386d8da9d86d5fee12ff12ac94dab1e94438d21
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-07-27 12:43:02 +0200

    Ejecutar todas las spider de datosSpider a la vez

--- Scrapy/datosEstaciones/spider_runner.py
+++ Scrapy/datosEstaciones/spider_runner.py
@@ -1,4 +1,9 @@
 from datosEstaciones.spiders.aemet_spider import AemetDataSpider
+from datosEstaciones.spiders.chcantabrico_pluvio_spider import ChcantabricoPluvioSpider
+from datosEstaciones.spiders.chcantabrico_nivel_spider import ChcantabricoNivelSpider
+from datosEstaciones.spiders.chcantabrico_coord_spider import ChcantabricoCoordSpider
+from datosEstaciones.spiders.meteoNavarra_coord_spider import MeteonavarraCoordSpider
+from datosEstaciones.spiders.meteoNavarra_spider import MeteonavarraDataSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
@@ -7,6 +12,11 @@ def main():
     settings = get_project_settings()
     process = CrawlerProcess(settings)
     process.crawl(AemetDataSpider)
+    process.crawl(ChcantabricoPluvioSpider)
+    process.crawl(ChcantabricoNivelSpider)
+    process.crawl(MeteonavarraDataSpider)
+    process.crawl(MeteonavarraCoordSpider)
+    process.crawl(ChcantabricoCoordSpider)
     process.start()
 
 
