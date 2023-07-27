7e3aa7968ff209c53a140a4ebd2de36213ca5d79
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-07-27 12:37:23 +0200

    Script funcional de envio de datos a Djnago

--- base_post_request.py
+++ base_post_request.py
@@ -1,7 +1,11 @@
+import json
+
 import requests
 
 url = "http://localhost:8000/polls/store"
-data = {'data': [{'key1': 'val1'}, {'key2': 'val2'}]}
+
+with open("JSONs/ParsedData/datos_aguaEnNavarra.json", encoding="utf-8") as f:
+    data = json.load(f)
 headers = {'content-type': 'application/json'}
 r = requests.post(url, json=data, headers=headers)
 #print(r.text)
