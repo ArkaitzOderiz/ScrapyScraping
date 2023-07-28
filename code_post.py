79d3cbf44833aa4331ca4cdd3e4d4f66ec5dba50
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-07-28 13:36:52 +0200

    Scripts envio post request de datos y codigos

--- code_post.py
+++ code_post.py
@@ -1,14 +1,14 @@
 import json
+import sys
 
 import requests
 
-url = "http://localhost:8000/polls/store"
+url = "http://localhost:8000/polls/storeCode"
 
-with open("JSONs/ParsedData/datos_aguaEnNavarra.json", encoding="utf-8") as f:
+with open(f"JSONs/ParsedData/codigos_{sys.argv[1]}.json", encoding="utf-8") as f:
     data = json.load(f)
 headers = {'content-type': 'application/json'}
 r = requests.post(url, json=data, headers=headers)
-#print(r.text)
 
 with open('response.html', 'w', encoding='utf-8') as f:
     f.write(r.text)
---
+++ data_post.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+import requests
+
+url = "http://localhost:8000/polls/storeData"
+
+with open(f"JSONs/ParsedData/datos_{sys.argv[1]}.json", encoding="utf-8") as f:
+    data = json.load(f)
+headers = {'content-type': 'application/json'}
+r = requests.post(url, json=data, headers=headers)
+
+with open('response.html', 'w', encoding='utf-8') as f:
+    f.write(r.text)
