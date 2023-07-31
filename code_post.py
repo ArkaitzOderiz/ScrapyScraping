c1ef8a99a5608077534f411c10258e39aa53c248
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-07-31 11:53:16 +0200

    correccion en direccion de directorio

--- code_post.py
+++ code_post.py
@@ -5,7 +5,7 @@ import requests
 
 url = "http://localhost:8000/polls/storeCode"
 
-with open(f"JSONs/RawCode/codigos_{sys.argv[1]}.json", encoding="utf-8") as f:
+with open(f"JSONs/ParsedCode/codigos_{sys.argv[1]}.json", encoding="utf-8") as f:
     data = json.load(f)
 headers = {'content-type': 'application/json'}
 r = requests.post(url, json=data, headers=headers)
