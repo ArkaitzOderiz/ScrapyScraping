82225858ecb21029305d8607a5dcccf669921ebd
Arkaitz Oderiz <oderiz.127292@e.unavarra.es>
2023-09-04 10:22:25 +0200

    Incluido bucle for olvidado

--- code_post.py
+++ code_post.py
@@ -5,10 +5,11 @@ import requests
 
 url = "http://localhost:8000/polls/storeCode"
 
-with open(f"JSONs/ParsedCode/codigos_{sys.argv[1]}.json", encoding="utf-8") as f:
-    data = json.load(f)
-headers = {'content-type': 'application/json'}
-r = requests.post(url, json=data, headers=headers)
+for i in sys.argv[1:]:
+    with open(f"Scrapy/{sys.argv[i]}Spider/JSONs/RefinedCode/codigos_{sys.argv[i]}.json", encoding="utf-8") as f:
+        data = json.load(f)
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, json=data, headers=headers)
 
-with open('response.html', 'w', encoding='utf-8') as f:
-    f.write(r.text)
+    with open('response.html', 'w', encoding='utf-8') as f:
+        f.write(r.text)
